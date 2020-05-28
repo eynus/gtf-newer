e59f2f507225a74e10cd5b15bf1c0069546f83e7
gistone_sunye <sunye@gistone.cn>
2020-05-28 18:39:05 +0800

    save

--- src/views/dataManage/index.vue
+++ src/views/dataManage/index.vue
@@ -24,7 +24,11 @@
         <!-- <div slot="trigger"></div> -->
       </Sider>
       <Layout class="h100">
-        <div :style="{ height: `calc(100% - ${remToPx(2.8)}px)` }" style="box-sizing: border-box;"  class="pd">
+        <div
+          :style="{ height: `calc(100% - ${remToPx(2.8)}px)` }"
+          style="box-sizing: border-box;"
+          class="pd"
+        >
           <div class="h100 card-style">
             <router-view />
           </div>
@@ -39,107 +43,109 @@
 </template>
 
 <script>
-  // @ is an alias to /src
-  import MyFooter from '@/components/MyFooter'
-  import { mapGetters } from 'vuex'
-  export default {
-    name: 'Home',
-    components: { MyFooter },
-    data() {
-      return {
-        activeMenuItem: 'overview',
-        dataManageList: [
-          {
-            name: '数据总览',
-            key: 'overview',
-            show: false,
-            icon: 'iconfont  icon-overview',
-            id: 'page_5_1'
-          },
-          {
-            name: '查询浏览',
-            key: 'query',
-            show: false,
-            icon: 'iconfont  icon-file',
-            id: 'page_5_2'
-          },
-          {
-            name: '服务管理',
-            key: 'service',
-            show: false,
-            icon: 'iconfont  icon-jiangbei',
-            id: 'page_5_3'
-          },
-          {
-            name: '质检管理',
-            key: 'inspection',
-            show: false,
-            icon: 'iconfont  icon-setting',
-            id: 'page_5_4'
-          }
-        ]
-      }
-    },
-    created() {
-    
-       this.activeMenuItem = this.$route.path.replace('/data/','').split('/')[0]
-      //判断当前用户是否有权限显示左侧列表menu
-      let dataSiderAuth = this.role.find(
-        (item, index) => item.resIdentif === 'main_menu_5'
-      ).childs
-      //设置show
-      dataSiderAuth.forEach((item, index) => {
-        let targetIndex = this.dataManageList.findIndex(
-          (data, idx) => data.id === item
-        )
-        this.dataManageList[targetIndex]['show'] = true
-      })
-    },
-    computed: {
-      ...mapGetters(['role']),
-      menuitemClasses() {
-        return ['menu-item', this.isCollapsed ? 'collapsed-menu' : '']
-      }
-    },
-    watch: {
-      '$route.path': function(newVal) {
-        switch (newVal) {
-          case '/data/query':
-          case '/data/inspection':
-          case '/data/service':
-          case '/data/overview':
-            this.activeMenuItem = newVal.slice(6)
-            break
-          default:
-            break
+// @ is an alias to /src
+import MyFooter from "@/components/MyFooter";
+import { mapGetters } from "vuex";
+export default {
+  name: "Home",
+  components: { MyFooter },
+  data() {
+    return {
+      activeMenuItem: "overview",
+      dataManageList: [
+        {
+          name: "数据总览",
+          key: "overview",
+          show: false,
+          icon: "iconfont  icon-overview",
+          id: "page_5_1"
+        },
+        {
+          name: "查询浏览",
+          key: "query",
+          show: false,
+          icon: "iconfont  icon-file",
+          id: "page_5_2"
+        },
+        {
+          name: "服务管理",
+          key: "service",
+          show: false,
+          icon: "iconfont  icon-jiangbei",
+          id: "page_5_3"
+        },
+        {
+          name: "质检管理",
+          key: "inspection",
+          show: false,
+          icon: "iconfont  icon-setting",
+          id: "page_5_4"
         }
+      ]
+    };
+  },
+  created() {
+    this.activeMenuItem = this.$route.path.replace("/data/", "").split("/")[0];
+    //判断当前用户是否有权限显示左侧列表menu
+    let dataSiderAuth = this.role.find(
+      (item, index) => item.resIdentif === "main_menu_5"
+    ).childs;
+    //设置show
+    dataSiderAuth.forEach((item, index) => {
+      let targetIndex = this.dataManageList.findIndex(
+        (data, idx) => data.id === item
+      );
+      this.dataManageList[targetIndex]["show"] = true;
+    });
+  },
+  computed: {
+    ...mapGetters(["role"]),
+    menuitemClasses() {
+      return ["menu-item", this.isCollapsed ? "collapsed-menu" : ""];
+    }
+  },
+  watch: {
+    "$route.path": function(newVal) {
+      switch (newVal) {
+        case "/data/query":
+        case "/data/inspection":
+        case "/data/service":
+        case "/data/overview":
+          this.activeMenuItem = newVal.slice(6);
+          break;
+        default:
+          break;
       }
-    },
-    methods: {
-      changeRoute(data) {
-        this.activeMenuItem = data
-        this.$router.push('/data/' + data)
+    }
+  },
+  methods: {
+    changeRoute(data) {
+      this.activeMenuItem = data;
+      if (data === "inspection") {
+        this.$router.push("/data/inspection/mathBasic?id=1");
+      } else {
+        this.$router.push("/data/" + data);
       }
     }
   }
+};
 </script>
 <style lang="scss" scoped>
-  .card-style {
-    background: #fff;
-    border-radius: 4px;
+.card-style {
+  background: #fff;
+  border-radius: 4px;
+}
+.menu-item {
+  & + .menu-item {
+    margin-top: 1.5rem;
   }
-  .menu-item {
-    & + .menu-item {
-      margin-top: 1.5rem;
-    }
-    text-align: center;
-    color: #8391b8;
-    padding: 10px 0;
-    // font-weight: 600;
-    div {
-      margin-top: 6px;
-      
-    }
+  text-align: center;
+  color: #8391b8;
+  padding: 10px 0;
+  // font-weight: 600;
+  div {
+    margin-top: 6px;
   }
+}
 </style>
 
