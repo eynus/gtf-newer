7711dfee737c744918e7ac1c95cf48a6ac3284fd
gistone_sunye <sunye@gistone.cn>
2020-06-15 18:28:17 +0800

    数据管理-upload

--- src/api/modelConfig/data.js
+++ src/api/modelConfig/data.js
@@ -4,3 +4,5 @@ import { baseUrl_model } from "@/api/set.js"
 export const getList = data => request({ url: baseUrl_model + 'api/model/list', method: 'post', data })
 // 获取所属类型列表
 export const getByLevel = data => request({ url: baseUrl_model + 'api/model/getByLevel', method: 'get', data })
+// 上传列表
+export const importData = data => request({ url: baseUrl_model + 'api/model/importData', method: 'post', data })
